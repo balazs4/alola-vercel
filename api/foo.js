b87e3d43c0e2034130118a78a3c705544b017dba
balazs4 <balazs4@users.noreply.github.com>
2021-04-26 23:03:30 +0200

    force new deployment

--- api/foo.js
+++ api/foo.js
@@ -4,6 +4,7 @@ module.exports = (req, res) => {
     .writeHead(200, {
       'content-type': 'application/json',
       'x-alola-vercel': 'yay',
+      'x-alola-vercel-trigger': 'foobar',
     })
     .end(
       JSON.stringify({
