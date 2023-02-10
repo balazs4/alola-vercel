79839cb5a22e199572e3c639bf4ffc8b2be9a560
balazs4 <balazs4@users.noreply.github.com>
2023-02-10 18:46:45 +0100

    edge (#2)
    feat: edge

--- api/foo.js
+++ api/foo.js
@@ -1,14 +1,11 @@
-module.exports = (req, res) => {
-  const { name = 'friend' } = req.query;
-  res
-    .writeHead(200, {
-      'content-type': 'application/json',
-      'x-alola-vercel': 'yay',
-    })
-    .end(
-      JSON.stringify({
-        name: name.toLowerCase(),
-        now: Date.now(),
-      })
-    );
-};
+export default function (req, event) {
+  const url = new URL(req.url);
+  const name = url.searchParams.get('name') || 'friend';
+
+  return new Response(
+    JSON.stringify({ name: name.toLowerCase(), now: Date.now() }),
+    { headers: { 'content-type': 'application/json', 'x-alola-vercel': 'yay' } }
+  );
+}
+
+export const config = { runtime: 'edge' };
