7de08a610d03170a883ad1503e61f286886421d0
balazs4 <balazs4@users.noreply.github.com>
2023-02-10 18:40:38 +0100

    yOlo cOmMit :sponge:

--- api/foo.js
+++ api/foo.js
@@ -1,5 +1,5 @@
 export default function (req, event) {
-  console.log(JSON.stringify({ req, event }));
+  console.log(req.url);
   return new Response(
     JSON.stringify({ name: 'friend'.toLowerCase(), now: Date.now() }),
     { headers: { 'content-type': 'application/json', 'x-alola-vercel': 'yay' } }
